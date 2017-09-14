57495c75543a7a855ee51d29823e9a12a72e5f6e
Philip Monk <phil@pcmonk.me>
2017-09-14 08:57:29 -0700

    don't sort for correlation test

--- originality.py
+++ originality.py
@@ -59,13 +59,13 @@ def is_almost_unique(submission_data, submission, db_manager, filemanager, is_ex
 
     date_created = db_manager.get_date_created(submission_data['submission_id'])
 
-    submission = np.sort(submission)
+    sorted_submission = np.sort(submission)
     for user_sub in db_manager.get_everyone_elses_recent_submssions(submission_data['competition_id'], submission_data['user'], date_created):
         with lock:
             other_submission = get_submission(db_manager, filemanager, user_sub["submission_id"])
         if other_submission is None:
             continue
-        score = originality_score(submission, other_submission)
+        score = originality_score(sorted_submission, other_submission)
 
         if is_not_a_constant and np.std(other_submission) > 0 :
             correlation = pearsonr(submission, other_submission)[0]
