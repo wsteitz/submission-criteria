4954e7e000e6daedf2da727b7b61252548600e98
Xander Dunn <xander@numer.ai>
2017-11-08 13:48:40 -0800

    Fix getting the API_KEY from S3 rather than using the default

--- server.py
+++ server.py
@@ -21,9 +21,11 @@ import concordance
 import common
 
 PORT = os.environ.get("PORT", "5151")
-API_KEY = os.environ.get("API_KEY", "h/52y/E7cm8Ih4F3cVdlBM4ZQxER+Apk6P0L7yR0lFU=")
+API_KEY = os.environ.get("API_KEY")
 if not API_KEY:
     API_KEY = common.get_secret("API_KEY")
+if not API_KEY:
+    API_KEY = "h/52y/E7cm8Ih4F3cVdlBM4ZQxER+Apk6P0L7yR0lFU="
 TEMP_DIR, OQ_DIR, CQ_DIR = "queue_temp", "oqueue", "cqueue"
 LB_TEMP_DIR, LBQ_DIR = "lb_temp", "lbqueue"
 
