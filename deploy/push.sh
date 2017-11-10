b51990bee9a2efc2e856ad6c249b8342b3dbe421
Xander Dunn <xander@numer.ai>
2017-11-10 03:59:46 -0800

    Remove debug statement

--- deploy/push.sh
+++ deploy/push.sh
@@ -3,8 +3,6 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR/../
 
-set -x
-
 docker build -t 074996771758.dkr.ecr.us-west-2.amazonaws.com/api-ml:$2 --build-arg env=$1 . &&
 aws --region us-west-2 ecr get-login --no-include-email | bash &&
 docker push 074996771758.dkr.ecr.us-west-2.amazonaws.com/api-ml:$2 &&
