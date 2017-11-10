629190038561d1be592898980e9a49399f5024b4
Xander Dunn <xander@numer.ai>
2017-11-10 03:17:15 -0800

    Fix CI

--- deploy/push.sh
+++ deploy/push.sh
@@ -4,7 +4,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR/../
 
 docker build -t 074996771758.dkr.ecr.us-west-2.amazonaws.com/api-ml:$2 --build-arg env=$1 . &&
-aws ecr get-login --no-include-email | sudo bash &&
+aws --region us-west-2 ecr get-login --no-include-email | sudo bash &&
 docker push 074996771758.dkr.ecr.us-west-2.amazonaws.com/api-ml:$2 &&
 ./deploy/gen-dockerrun.sh $2 &&
 eb deploy --staged $2
